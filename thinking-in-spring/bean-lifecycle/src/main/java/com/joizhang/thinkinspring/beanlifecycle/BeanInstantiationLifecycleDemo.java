230034f3c1924965c23ba7b19be72c6137e262cd
joizhang <zxlwhom@outlook.com>
2023-03-16 15:45:25 +0800

    feat: Bean 元信息配置示例，注解 BeanDefinition 解析示例，BeanDefinition 合并示例

---
+++ thinking-in-spring/bean-lifecycle/src/main/java/com/joizhang/thinkinspring/beanlifecycle/BeanInstantiationLifecycleDemo.java
@@ -0,0 +1,38 @@
+package com.joizhang.thinkinspring.beanlifecycle;
+
+import com.joizhang.thinkinginspring.iocoverview.domain.User;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+/**
+ * Bean 实例化生命周期示例
+ */
+public class BeanInstantiationLifecycleDemo {
+
+    public static void main(String[] args) {
+        executeBeanFactory();
+        System.out.println("--------------------------------");
+    }
+
+    private static void executeBeanFactory() {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        // 方法一：添加 BeanPostProcessor 实现 MyInstantiationAwareBeanPostProcessor
+        // beanFactory.addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
+        // 方法二：将 MyInstantiationAwareBeanPostProcessor 作为 Bean 注册
+        // 基于 XML 资源 BeanDefinitionReader 实现
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+        String[] locations = {"META-INF/dependency-lookup-context.xml", "META-INF/bean-constructor-dependency-injection.xml"};
+        int beanNumbers = beanDefinitionReader.loadBeanDefinitions(locations);
+        System.out.println("已加载 BeanDefinition 数量：" + beanNumbers);
+        // 通过 Bean Id 和类型进行依赖查找
+        User user = beanFactory.getBean("user", User.class);
+        System.out.println(user);
+
+        User superUser = beanFactory.getBean("superUser", User.class);
+        System.out.println(superUser);
+
+        // 构造器注入按照类型注入，resolveDependency
+        UserHolder userHolder = beanFactory.getBean("userHolder", UserHolder.class);
+        System.out.println(userHolder);
+    }
+}
