c86a0a9ca4f738199c6db51369509a7149332041
joizhang <zxlwhom@outlook.com>
2023-03-16 15:41:00 +0800

    feat: 限定注入

---
+++ thinking-in-spring/dependency-injection/src/main/java/com/joizhang/thinkinginspring/di/annotation/UserGroup.java
@@ -0,0 +1,22 @@
+package com.joizhang.thinkinginspring.di.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+/**
+ * 用户组注解，扩展 {@link Qualifier}
+ */
+@Target({
+        ElementType.FIELD,
+        ElementType.METHOD,
+        ElementType.PARAMETER,
+        ElementType.TYPE,
+        ElementType.ANNOTATION_TYPE
+})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Qualifier
+public @interface UserGroup {
+}
